c1abfc475a18523cd02816947c19a2cccc660c64
ConnerChang <edi7709@gamil.com>
2016-05-26 13:18:12 +0800

    initial commit

---
+++ R/stock_ai.R
@@ -0,0 +1,20 @@
+# get data from https://stock-ai.com/
+library(httr)
+library(jsonlite)
+
+get_data_from_stock_ai <- function(symbol_code, start_year, start_month, end_year, end_month) {
+  url <- "https://stock-ai.com/eomDataQuery.php"
+  body = list(a = "c",
+              showType = "Value",
+              symbolCode = symbol_code,
+              startYear = start_year,
+              startMonth = start_month,
+              endYear = end_year,
+              endMonth = end_month,
+              hash = "d41d8cd98f00b204e9800998ecf8427e") # need to check whether the hash code will change
+
+  res = POST(url, body = body, encode = "form")
+  res_str <- content(res, as = "text", encoding = "utf8")
+
+  res_str %>% fromJSON() %>% .$row
+}
